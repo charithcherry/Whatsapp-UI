b821179fdbda03972d12b6618955bc88e73714b6
charith <charithcherry1100gmail.com>
2020-11-17 20:15:49 +0530

    first 59

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAoEOiRU-Dfq1YMYr2cI05kX2Bgr7Aorcg",
+    authDomain: "watsapp-mern-4d5ce.firebaseapp.com",
+    databaseURL: "https://watsapp-mern-4d5ce.firebaseio.com",
+    projectId: "watsapp-mern-4d5ce",
+    storageBucket: "watsapp-mern-4d5ce.appspot.com",
+    messagingSenderId: "799666162046",
+    appId: "1:799666162046:web:fb23231febd469746f4b2f"
+  };
+
+  
