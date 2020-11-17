cb6239558adb6c2d830043d02cb78333904efc53
charith <charithcherry1100gmail.com>
2020-11-17 21:35:10 +0530

    before chat section 107

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from 'react';
 import './Sidebar.css';
-import DonutLargeIcon from "@material-ui/icons/DonutLarge"
-import ChatIcon from '@material-ui/icons/Chat'
-import MoreVertIcon from "@material-ui/icons/MoreVert"
-import { IconButton, Avatar, Icon } from '@material-ui/core';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { IconButton, Avatar } from '@material-ui/core';
+import { SearchOutlined} from "@material-ui/icons";
+
+
 function Sidebar() {
     return (
         <div className="sidebar">
         <div className="sidebar__header">
             
-            <Avatar src=""/>
+            <Avatar src="https://raw.githubusercontent.com/sitebase/react-avatar/HEAD/docs/example1.jpg"/>
             <div className="sidebar__headerRight">
               <IconButton>
               <DonutLargeIcon/>
@@ -23,8 +26,14 @@ function Sidebar() {
             </div>
         </div>
 
+        <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+            <SearchOutlined/>
+            <input placeholder="Search or start new chat" type="text"/>
         </div>
-    )
+        </div>
+        </div>
+    );
 }
 
 export default Sidebar
