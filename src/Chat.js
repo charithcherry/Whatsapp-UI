ae0e8387fdfc00f5d33cf835fe3e3958e32bab65
charith <charithcherry1100gmail.com>
2020-11-19 01:33:03 +0530

    backend #150

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,74 @@
-import React from 'react'
-import './Chat.css'
+import { Avatar , IconButton} from '@material-ui/core'
+import { AttachFile, SearchOutlined ,MoreVert} from '@material-ui/icons'
+import React from 'react';
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon"
+import MicIcon from "@material-ui/icons/Mic"
+import './Chat.css';
 function Chat() {
     return (
         <div className="chat">
-            <h1>i am chat</h1>
+ <div className="chat__header">
+     <Avatar/>
+     <div className="chat__headerInfo">
+         <h3>Room name</h3>
+         <p>room info last seen..</p>
+     </div>
+     <div className="chat__headerRight">
+     <IconButton>
+              <SearchOutlined/>
+               </IconButton>
+               <IconButton>
+                <AttachFile/>
+               </IconButton>
+               <IconButton>
+                <MoreVert/>
+               </IconButton>     </div>
+ </div>
+   <div className="chat__body">
+      <p className="chat__message">
+      <span className="chat__name">
+          Mahesh</span>    
+          this is a message
+          <span className="chat__timestamp">
+              {new Date().toUTCString()}
+          </span>
+      </p> 
+      <p className=" chat__message chat__receiver">
+      <span className="chat__name">
+          Mahesh</span>    
+          this is a message
+          <span className="chat__timestamp">
+              {new Date().toUTCString()}
+          </span>
+      </p> 
+      <p className="chat__message">
+      <span className="chat__name">
+          Mahesh</span>    
+          this is a message
+          <span className="chat__timestamp">
+              {new Date().toUTCString()}
+          </span>
+      </p> 
+       
+       
+       </div>
+    <div className="chat__footer">
+    <InsertEmoticonIcon/>
+    <form >
+        <input 
+        placeholder="Type a message"
+        type="text"/>
+        <button 
+        type="submit">
+        Send Message
+        </button>
+    </form>
+    <MicIcon/>
+    </div>
+
+
+
+
         </div>
     )
 }
